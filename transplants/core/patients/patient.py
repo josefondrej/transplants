961c5efd23fdf81096e4b735dc0fcb4c4abc2d7b
Josef Ondrej <josef.ondrej@outlook.com>
2020-09-25 21:56:01 +0200

    Add patient id

--- transplants/core/patients/patient.py
+++ transplants/core/patients/patient.py
@@ -2,5 +2,6 @@ from transplants.core.patients.patient_data.patient_data import PatientData
 
 
 class Patient:
-    def __init__(self, patient_data: PatientData):
+    def __init__(self, identifier: str, patient_data: PatientData):
+        self._identifier = identifier
         self._patient_data = patient_data
--- transplants/core/patients/recipient.py
+++ transplants/core/patients/recipient.py
@@ -6,6 +6,6 @@ from transplants.core.patients.patient_data.patient_data import PatientData
 
 
 class Recipient(Patient):
-    def __init__(self, patient_data: PatientData, related_donors: List[Donor]):
+    def __init__(self, identifier: str, patient_data: PatientData, related_donors: List[Donor]):
         self._related_donors = related_donors
-        super().__init__(patient_data)
+        super().__init__(identifier=identifier, patient_data=patient_data)
