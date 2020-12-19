791baeeeed04f90cbe082b82e83d8780a004e67b
Josef Ondrej <josef.ondrej@outlook.com>
2020-12-19 11:41:01 +0100

    Refactor forbidden blood type from MedicalData to BloodType

--- transplants/core/patient/medical_data/blood_group_system/blood_type.py
+++ transplants/core/patient/medical_data/blood_group_system/blood_type.py
@@ -1,13 +1,38 @@
+from typing import Dict, Set, Union
+
 from transplants.core.patient.medical_data.antigen_antibody_system.antigen_antibody_system import AntigenAntibodySystem
+from transplants.core.patient.medical_data.blood_group_system.blood_type_antibody import BloodTypeAntibody
+from transplants.core.patient.medical_data.blood_group_system.blood_type_antigen import BloodTypeAntigen
 
 
 class BloodType(AntigenAntibodySystem):
+    """Blood type of patient
+
+    Args:
+        antigens: Set of antigens
+        antibodies: Set of antibodies
+        forbidden_blood_types: Set of blood types of donors from who the patient can't receive transplant even
+            if we allow transplants across blood type
+    """
+
+    def __init__(self, antigens: Set[BloodTypeAntigen],
+                 antibodies: Union[Set[BloodTypeAntibody], Dict[BloodTypeAntibody, float]],
+                 forbidden_blood_types: Set["BloodType"] = None):
+        super().__init__(antigens=antigens, antibodies=antibodies)
+        self._forbidden_blood_types = forbidden_blood_types
+
     def can_give_to(self, recipient_blood_type: "BloodType") -> bool:
+        """Can patient with this blood type give blood to patient with recipient_blood_type"""
         return not recipient_blood_type.has_antibodies_for(self.antigens)
 
     def can_receive_from(self, donor_blood_type: "BloodType") -> bool:
+        """Can patient with this blood type receive blood from patient with donor_blood_type"""
         return donor_blood_type.can_give_to(self)
 
     def __str__(self) -> str:
         representation = "".join(sorted(map(str, self.antigens)))
         return "0" if len(representation) == 0 else representation
+
+    @property
+    def forbidden_blood_types(self) -> Set["BloodType"]:
+        return self._forbidden_blood_types
--- transplants/core/patient/medical_data/medical_data.py
+++ transplants/core/patient/medical_data/medical_data.py
@@ -1,5 +1,3 @@
-from typing import Set, Optional
-
 from transplants.core.patient.medical_data.blood_group_system.blood_type import BloodType
 from transplants.core.patient.medical_data.hla_system.hla_system import HLASystem
 
@@ -15,11 +13,9 @@ class MedicalData:
                 even if we allow transplant across blood group
     """
 
-    def __init__(self, blood_type: BloodType, hla_system: HLASystem,
-                 forbidden_blood_types: Optional[Set[BloodType]] = None):
+    def __init__(self, blood_type: BloodType, hla_system: HLASystem):
         self._blood_type = blood_type
         self._hla_system = hla_system
-        self._forbidden_blood_types = forbidden_blood_types
 
     @property
     def blood_type(self) -> BloodType:
@@ -28,7 +24,3 @@ class MedicalData:
     @property
     def hla_system(self) -> HLASystem:
         return self._hla_system
-
-    @property
-    def forbidden_blood_types(self) -> Optional[Set[BloodType]]:
-        return self._forbidden_blood_types
